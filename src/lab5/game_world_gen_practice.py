86ccaca27ccc8d78b2eb4e0020551857c47fc001
ghgsrt <ajb7366@psu.edu>
2023-02-07 17:02:37 -0500

    Completed Lab5

--- src/lab5/game_world_gen_practice.py
+++ src/lab5/game_world_gen_practice.py
@@ -18,17 +18,45 @@ You can always write non-modular code for the first time and then refactor it la
 import sys
 import pygame
 import random
+import itertools as it
 import numpy as np
 from landscape import get_landscape
 
 from pathlib import Path
 sys.path.append(str((Path(__file__)/'..'/'..').resolve().absolute()))
 from lab2.cities_n_routes import get_randomly_spread_cities, get_routes
-
+from lab3.travel_cost import route_to_coordinates
 
 # TODO: Demo blittable surface helper function
 
 ''' Create helper functions here '''
+
+def draw_nodes(locs, node_dims, color):
+    for loc in locs:
+        pygame.draw.rect(screen, color, [*loc, node_dims, node_dims ])
+
+def get_full_route(route_coordinate):
+    def sign(num):
+        return 1 if num > 0 else -1 if num < 0 else 0
+
+    start_cell, end_cell = route_coordinate
+    dist_x, dist_y = (end_cell[0] - start_cell[0], end_cell[1] - start_cell[1])
+    sign_x, sign_y = (sign(dist_x), sign(dist_y))
+    num_moves_x, num_moves_y = (abs(dist_x), abs(dist_y))
+
+    it_num_moves = it.zip_longest(
+        range(num_moves_x),
+        range(num_moves_y),
+        fillvalue=min(num_moves_x, num_moves_y)
+    )
+
+    return [
+        (
+            (start_cell[0] + (sign_x * i)),
+            (start_cell[1] + (sign_y * j))
+        )
+        for i, j in it_num_moves
+    ]
 
 if __name__ == "__main__":
     pygame.init()
@@ -38,29 +66,36 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode(size)
     landscape = get_landscape(size)
     print("Created a landscape of size", landscape.shape)
-    pygame_surface = pygame.surfarray.make_surface(landscape[:, :, :3]) 
+    pygame_surface = pygame.surfarray.make_surface(landscape[:, :, :3])
 
     city_names = ['Morkomasto', 'Morathrad', 'Eregailin', 'Corathrad', 'Eregarta',
                   'Numensari', 'Rhunkadi', 'Londathrad', 'Baernlad', 'Forthyr']
-    city_locations = [] 
-    routes = []
+    city_locations = get_randomly_spread_cities(size, len(city_names))
+    routes = get_routes(city_names)
 
     ''' Setup cities and routes in here'''
 
-    city_locations_dict = {name: location for name, location in zip(city_names, city_locations)}
+    # city_locations_dict = {name: location for name, location in zip(city_names, city_locations)}
     random.shuffle(routes)
-    routes = routes[:10] 
+    routes = routes[:10]
+
+    route_coordinates = route_to_coordinates(city_locations, city_names, routes)
+    route_paths = [get_full_route(coord) for coord in route_coordinates]
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill(black)
+        screen.fill('black')
         screen.blit(pygame_surface, (0, 0))
 
         ''' draw cities '''
+        draw_nodes(city_locations, 15, 'black')
 
         ''' draw first 10 routes '''
+        for path in route_paths:
+            draw_nodes(path, 4, '#9C661F')
 
+        pygame.display.update()
         pygame.display.flip()
--- src/lab5/landscape.py
+++ src/lab5/landscape.py
@@ -4,20 +4,34 @@ import numpy as np
 
 def get_elevation(size):
     xpix, ypix = size
-    elevation = np.array([])
-    '''Play around with perlin noise to get a better looking landscape (This is required for the lab)'''
 
-    return elevation
+    noise1 = PerlinNoise(octaves=3)
+    noise2 = PerlinNoise(octaves=6)
+    noise3 = PerlinNoise(octaves=12)
+    noise4 = PerlinNoise(octaves=24)
+
+    noise = []
+    for i in range(xpix):
+        row = []
+        for j in range(ypix):
+            noise_val = noise1([i/xpix, j/ypix])
+            noise_val += 0.5 * noise2([i/xpix, j/ypix])
+            noise_val += 0.25 * noise3([i/xpix, j/ypix])
+            noise_val += 0.125 * noise4([i/xpix, j/ypix])
+
+            row.append(noise_val)
+        noise.append(row)
+
+    return np.array(noise)
 
 def elevation_to_rgba(elevation):
     xpix, ypix = np.array(elevation).shape
-    colormap = plt.cm.get_cmap('gist_earth')
+    colormap = plt.cm.get_cmap('jet')
     elevation = (elevation - elevation.min())/(elevation.max()-elevation.min())
     ''' You can play around with colormap to get a landscape of your preference if you want '''
     landscape = np.array([colormap(elevation[i, j])[0:3] for i in range(xpix) for j in range(ypix)]).reshape(xpix, ypix, 3)*255
     landscape = landscape.astype('uint8')
     return landscape
- 
 
 get_landscape = lambda size: elevation_to_rgba(get_elevation(size))
 
@@ -25,5 +39,5 @@ get_landscape = lambda size: elevation_to_rgba(get_elevation(size))
 if __name__ == '__main__':
     size = 640, 480
     pic = elevation_to_rgba(get_elevation(size))
-    plt.imshow(pic, cmap='gist_earth')
+    plt.imshow(pic, cmap='gray')
     plt.show()
